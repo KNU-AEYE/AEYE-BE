69aec727b8acbe25752754fbd5b25c7ef9d75ff7
Sion Shin <kmicety1@gmail.com>
2024-03-21 23:19:08 +0900

    Project initialize

---
+++ src/test/java/com/server/aeye/AeyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.aeye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
