63a5256e0df82887f0398808b6dadccaf749135d
Sion Shin <kmicety1@gmail.com>
2024-03-22 17:39:44 +0900

    feat: add Google OAuth2 configuration

---
+++ src/main/java/com/server/aeye/oauth/provider/GoogleOAuth2UserInfo.java
@@ -0,0 +1,28 @@
+package com.server.aeye.oauth.provider;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo extends OAuth2UserInfo {
+
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    public String getPicture() {
+        return (String) attributes.get("picture");
+    }
+}
