9e2fd679d6eb20803b48a89f6e178be4df7fcc17
Sion Shin <kmicety1@gmail.com>
2024-05-19 16:32:26 +0900

    feat: SendGrid 서비스 추가

---
+++ src/main/java/com/server/aeye/controller/SendGridController.java
@@ -0,0 +1,35 @@
+package com.server.aeye.controller;
+
+import com.server.aeye.DTO.ApiResponseDto;
+import com.server.aeye.exception.SuccessStatus;
+import com.server.aeye.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sendgrid")
+@RequiredArgsConstructor
+@SecurityRequirement(name = "Access Token")
+@Tag(name = "SendGrid", description = "SendGrid API")
+public class SendGridController {
+
+    private final EmailService emailService;
+
+    // SendGrid API를 사용하여 이메일을 전송합니다.
+    @Operation(summary = "이메일 전송", description = "SendGrid API를 사용하여 이메일을 전송합니다.")
+    @PostMapping
+    public ApiResponseDto<?> sendEmail(@Parameter(hidden = true) @AuthenticationPrincipal User user)
+        throws IOException {
+        emailService.sendEmail();
+        return ApiResponseDto.success(SuccessStatus.OK, SuccessStatus.OK.getMessage());
+    }
+}
