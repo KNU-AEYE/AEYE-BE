5e20802f62ef7fe177d59aca0953e7ac25f5e08a
Sion Shin <kmicety1@gmail.com>
2024-05-20 22:40:10 +0900

    feat: PDF 생성

--- src/main/java/com/server/aeye/service/ReportService.java
+++ src/main/java/com/server/aeye/service/ReportService.java
@@ -1,7 +1,14 @@
 package com.server.aeye.service;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.server.aeye.domain.Report;
 import com.server.aeye.infrastructure.ReportRepository;
+import com.server.aeye.util.DataBucketUtil;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,14 +18,33 @@ import org.springframework.stereotype.Service;
 public class ReportService {
 
     private final ReportRepository reportRepository;
+    private final DataBucketUtil dataBucketUtil;
 
-    public Report generateDailyReport() {
+    public Report generateDailyReport() throws IOException, DocumentException {
         // 일일 관제일지 생성
+        byte[] pdfData = createDailyReportPdf();
+
+        String reportUri = dataBucketUtil.uploadDailyReport(pdfData, "application/pdf");
         Report report = Report.builder()
             .date(LocalDate.now())
-            .reportUri("https://storage.googleapis.com/aeye-bucket/report/report_sample.pdf")
+            .reportUri(reportUri)
             .build();
         return reportRepository.save(report);
+    }
+
+    private byte[] createDailyReportPdf() throws DocumentException {
+        Document document = new Document();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PdfWriter.getInstance(document, baos);
+
+        document.open();
+        document.add(new Paragraph("Daily Report"));
+
+        // PDF 내용 추가
+
+        document.close();
+
+        return baos.toByteArray();
     }
 
 }
