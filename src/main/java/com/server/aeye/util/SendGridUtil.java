b27ecc7550be06f6b8d68ae0b4398d947a3664e6
Sion Shin <kmicety1@gmail.com>
2024-05-20 14:07:25 +0900

    feat: sendDynamicTemplateEmailv2

--- src/main/java/com/server/aeye/util/SendGridUtil.java
+++ src/main/java/com/server/aeye/util/SendGridUtil.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -20,10 +21,16 @@ public class SendGridUtil {
 
     private final SendGrid sendGrid;
 
+    @Value("${spring.sendgrid.from}")
+    private String fromEmail;
+
+    @Value("${spring.sendgrid.template-id}")
+    private String templateId;
+
     // Sendgrid 공식 가이드 참고
     // https://github.com/sendgrid/sendgrid-java
     public void sendEmail() throws IOException {
-        Email from = new Email("kmicety1@gmail.com");
+        Email from = new Email(fromEmail);
         String subject = "Sending with Twilio SendGrid is Fun";
         Email to = new Email("kmicety1@gmail.com");
         Content content = new Content("text/plain", "and easy to learn");
@@ -31,6 +38,24 @@ public class SendGridUtil {
         Mail mail = new Mail(from, subject, to, content);
         Email email = new Email("yelnets99@naver.com");
         mail.personalization.get(0).addTo(email);
+
+        send(mail);
+    }
+
+    public void sendDynamicTemplateEmailv2(Member member, Report report) throws IOException {
+        Email from = new Email(fromEmail);
+        Email to = new Email(member.getEmail());
+        Mail mail = new Mail();
+
+        mail.setFrom(from);
+        mail.setTemplateId(templateId);
+
+        Personalization personalization = new Personalization();
+        personalization.addDynamicTemplateData("name", member.getName());
+        personalization.addDynamicTemplateData("link", report.getReportUri());
+        personalization.addTo(to);
+
+        mail.addPersonalization(personalization);
 
         send(mail);
     }
